c808fa6c930d11dc4cb2bedfbff246696886148d
eddvalles <eddvalles201@gmail.com>
2026-03-23 20:28:16 -0400

    Review of multi-dimensional and variable length arrays

---
+++ 6_Arrays/Fig_6_23.c
@@ -0,0 +1,32 @@
+// Fig. 6.23: fig06_23.c
+// Using variable length arrays in C99
+#include <stdio.h>
+
+// function prototypes
+void print1DArray(size_t size, int array[size]);
+void print2DArray(size_t row, size_t col, int array[row][col]);
+
+int main(void)
+{
+    printf("%s", "Enter size of a one-dimensional array: ");
+}
+
+void print1DArray(size_t size, int array[size])
+{
+    // output contents of array
+    for (size_t i = 0; i < size; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
+}
+
+void print2DArray(size_t row, size_t col, int array[row][col])
+{
+    // output contents of array
+    for (size_t i = 0; i < row; ++i) {
+        for (size_t j = 0; j < col; ++j) {
+            printf("%5d", array[i][j]);
+        }
+
+        puts("");
+    }
+}
