79e422565dcdb607c2bb7c98a058c5630252ddc0
eddvalles <eddvalles201@gmail.com>
2026-03-27 16:01:05 -0400

    Add Fig 6.23: variable length arrays with runtime-size 1D and 2D arrays

--- 6_Arrays/Fig_6_23.c
+++ 6_Arrays/Fig_6_23.c
@@ -17,6 +17,49 @@ int main(void)
     printf("%s", "Enter number of rows and columns in a 2-D array: ");
     int row1, col1; // number of rows and columns in a 2-D array
     scanf("%d %d", &row1, &col1);
+
+    int array2D1[row1][col1]; // declare 2-D variable-length array
+
+    printf("%s",
+        "Enter number of rows and columns in another 2-D array: ");
+    int row2, col2; // number of rows and columns in another 2-D array
+    scanf("%d %d", &row2, &col2);
+
+    int array2D2[row2][col2]; // declare 2-D variable-length array
+
+    // test sizeof operator on VLA
+    printf("\nsizeof(array) yields array size of %d bytes\n",
+        sizeof(array));
+    
+    // assign elements of 1-D VLA
+    for (size_t i = 0; i < arraySize; ++i){
+        array[i] = i * i;
+    }
+
+    // assign elements of first 2-D VLA
+    for (size_t i = 0; i < row1; ++i){
+        for (size_t j = 0; j < col1; ++j){
+            array2D1[i][j] = i + j;
+        }
+    }
+
+    // assign elements of 2-D VLA
+    for (size_t i = 0; i < row2; ++i){
+        for (size_t j = 0; j < col2; ++j){
+            array2D2[i][j] = i + j;
+        }
+    }
+
+    puts("\nOne-dimensional array:");
+    print1DArray(arraySize, array); // pass 1-D VLA to function
+
+    puts("\nFirst two-dimensional array:");
+    print2DArray(row1, col1, array2D1); // pass 2-D VLA to function
+
+    puts("\nSecond two-dimensional array:");
+    print2DArray(row2, col2, array2D2); // pass other 2-D VLA to function
+
+    return 0;
 }
 
 void print1DArray(size_t size, int array[size])
