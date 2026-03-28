c854569ca05760773f47ca2cff8fdbbd467d2428
eddvalles <eddvalles201@gmail.com>
2026-03-28 14:00:58 -0400

    Add Fig 7.7: cube a variable using pass-by-reference with a pointer argument

---
+++ 7_C_Pointers/Fig_7_7.c
@@ -0,0 +1,26 @@
+// Fig. 7.7: fig07_07.c
+// Cube a variable using pass-by-reference with a pointer argument.
+
+#include <stdio.h>
+
+void cubeByReference(int *nPtr); // function prototype
+
+int main(void)
+{
+    int number = 5;
+
+    printf("The original value of number is %d", number);
+
+    // pass address of number to cubeByReference
+    cubeByReference(&number);
+
+    printf("\nThe new value of number is %d\n", number);
+
+    return 0;
+}
+
+// calculate cube of *nPtr; actually modifies number in main
+void cubeByReference(int *nPtr)
+{
+    *nPtr = *nPtr * *nPtr * *nPtr; // cube *nPtr
+}
