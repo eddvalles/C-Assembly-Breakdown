72855cd5ba8e135ae1fdf76c8158efaf37ec0ffe
eddvalles <eddvalles201@gmail.com>
2026-04-08 20:17:08 -0400

    Add Fig 8.12 and Fig 8.13 sprintf and sscanf

---
+++ 8_C_Characters_and_Strings/Fig_8_12.c
@@ -0,0 +1,20 @@
+// Fig. 8.12: fig08_12.c
+// Using function sprintf
+#include <stdio.h>
+#define SIZE 80
+
+int main(void)
+{
+    int x; // x value to be input
+    double y; // y value to be input
+
+    puts("Enter an integer and a double:");
+    scanf("%d%lf", &x, &y);
+
+    char s[SIZE]; // create char array
+    sprintf(s, "integer:%6d\ndouble:%7.2f", x, y);
+
+    printf("%s\n%s\n", "The formatted output stored in array s is:", s);
+
+    return 0;
+}
